31e5c67611fec82d92cc59fa8cd867e8d9da2d05
Luke Ishag <lukeishag@infra04-wg012.eduroam.wireless.umn.edu>
2024-09-23 20:12:34 -0500

    autograder.c now reads from submissions.txt and creates child processes that print out the name and then exit

--- autograder.c
+++ autograder.c
@@ -21,12 +21,48 @@ int main(int argc, char *argv[]) {
     // write the file paths from the "solutions" directory into the submissions.txt file
     write_filepath_to_submissions("solutions", "submissions.txt");
 
-
     //TODO: read the executable filename from submissions.txt
+    FILE *file; 
+    file = fopen("submissions.txt", "r");
 
+    //hard coded but we can change this later
+    int total_executables = 20;
 
-    //TODO: For each parameter, run all executables in batch size chunks
-    
+    int L = argc - 2; //L = length of [p1,p2,...,pn]
+    //Loop 1 time for each parameter input by user
+    for(int i = 0; i<L; i++){
+        int done_executables = 0;
+        while(done_executables<total_executables){
+            pid_t *pid_list = malloc(batch_size * sizeof(pid_t)); 
+            for(int j = 0; j<batch_size; j++){
+                pid_list[j] = fork();
+                if (pid_list[j]<0){
+                    perror("Fork Failed");
+                    exit(EXIT_FAILURE);
+                }else if (pid_list[j] == 0){
+                    //TODO: For each parameter, run all executables in batch size chunks          
+                    // This will run once for each name
+                    char* filename;
+                    fgets(filename, 100, file);
+                    printf("Child process: I would run filename '%s'.\n",filename);
+                    exit(0); 
+                }
+            }
+
+            int finished = 0;
+            //ensure all from this batch are done
+            while(finished<batch_size){
+                int status; //we will need to store this somewhere, and check exit statuses below
+                waitpid(pid_list[finished], &status, 0);
+                printf("Child exited\n");
+                finished++;
+            }
+            //We finished a batch, so add that many to done executables
+            done_executables += batch_size;
+            free(pid_list);
+        }
+    }
+    fclose(file);
     
     //TODO: Write the status of each executable file from "submissions.txt" to autograder.out. For testing purposes, you can compare the results with the provided expected.out file
     print_status();    
